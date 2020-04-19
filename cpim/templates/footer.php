1288e5c2682d1b836abd40cd98e17f11f4ce5849
esitamon <sitamoe_2010@yahoo.fr>
2020-04-19 12:39:14 -0500

    commite after adding basic content including some picture

--- cpim/templates/footer.php
+++ cpim/templates/footer.php
@@ -1,7 +1,31 @@
 
 
 <footer>
- <p><h1>je suis ton footer</h1></p>
+<section class="content3">
+    <div class="logo-footer">
+        <img src="cpim\img\logo.png" alt="">
+    </div>
+    <div class="logo-description">
+        <p>Experiencing his love, Displaying his power</p> 
+    </div>
+    <div class="footer-nav">
+        <ul>
+            <li><span>Connect</span>/li> 
+            <li><a href="">Join group</a></li>
+        </ul>
+        <ul>
+            <li>Grow</li>
+            <li><a href="">Kids</a></li>
+            <li><a href="">Events</a></li>
+            <li><a href="">Classes</a></li>
+        </ul>
+        <ul>
+            <li>About us</li>
+            <li><a href="">Our story</a></li>
+            <li><a href="">Contact us</a></li>
+        </ul>
+    </div>
+</section>
 </footer>
 </body>
 <script src="cpim\js\script.js"></script>
--- index.php
+++ index.php
@@ -1,5 +1,84 @@
 <?php include './cpim/templates/header.php';?>
 
-    <p>uouoi</p>
+<section class="content1">
+    <div class="logo">
+        <img src="cpim\img\logo.png" width="12%" alt="logo">
+    </div>
+    <nav>
+        <ul>
+            <li><a href="http://">Home</a></li>
+            <li><a href="http://">About us</a></li>
+            <li><a href="http://">Connect</a></li>
+            <li><a href="http://">Get in touch</a></li>
+        </ul>
+        <div class="button-give">
+            <p><a href="">Give</a></p>
+        </div>
+    </nav>
+    <video class="video" autoplay muted loop id="myVideo">
+        <source src="cpim\img\water.mp4" type="video/mp4">
+    </video>
+    <div class="overlay">
+        <div>
+            <p><h1>Welcome to the</h1></p>
+            <p><h1>Christ palace international</h1></p>
+        </div>
 
+    </div>
+    <div class="media-button">
+        <div class="our-pastor"> <p>About Our Pastor</p></div>
+        <div class="youtube"><p>Watch us on</p></div>
+        <div class="visit-us"><p>Visit us</p></div>
+    </div>
+    
+</section>
+<section class="content2">
+    <div class="sermon">
+        <p>Latest Sermons</p>
+    </div>
+    <div class="video"></div>
+    <div class="video"></div>
+    <div class="video"></div>
+    <div class="video"></div>
+    <div class="video"></div>
+    <div class="video"></div>
+    <div class="see-more">
+        <span>
+        see more>
+        </span>
+    </div>
+    <div class="pastor-description">
+        <div class="description-text">
+
+        </div>
+        <div class="pastor-picture">
+            <img src="cpim\img\MARK11.jpg" width="20%" alt="">
+        </div>
+    </div>
+    <div class="visiting-info">
+        <div class="address">
+            <div class="overlay-address">
+                <p>Adress</p>
+                <span>
+                    6220 alder dr, Houston, TX 77081
+                </span>
+            </div>
+        </div>
+        <div class="services-hours">
+            <div class="overlay-services">
+
+            </div>
+            <p>Services</p>
+            <span> Sunday 10:30pm<br> Wednesday 7:30pm</span>
+        </div>
+        <div class="phone-email">
+            <div class="overlay-contact">
+
+            </div>
+            <p>Phone & Email</p>
+            <span>+3462883856<br> christpalace@gmail.com</span>
+        </div>
+    </div>
+
+</section>
 <?php include './cpim/templates/footer.php' ?>
