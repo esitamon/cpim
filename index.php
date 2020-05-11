1d6b7741b65a38a22c9e0afc662f31232d21bfad
esitamon <sitamoe_2010@yahoo.fr>
2020-05-11 01:02:56 -0500

    laptop commit 2

--- index.php
+++ index.php
@@ -2,108 +2,111 @@
 
 <section class="wrapper">
 
-<section class="content1">
-    <div class="logo">
-        <img src="cpim\img\logo.png" width="12%" alt="logo">
-    </div>
-    <nav>
-        <ul>
-            <li><a href="http://">Home</a></li>
-            <li><a href="http://">About us</a></li>
-            <li><a href="http://">Connect</a></li>
-            <li><a href="http://">Get in touch</a></li>
-        </ul>
-    </nav>
-    <div class="button-give">
-            <p><a href="">Give</a></p>
-    </div>
-    <video class="myvideo" autoplay muted loop id="myVideo">
-        <source src="cpim\img\water.mp4" type="video/mp4">
-    </video>
-    <div class="overlay">
-        <div>
-            <p>Welcome to the</p>
-            <p id="x">Christ palace international</p>
+    <section class="content1">
+        <div class="logo">
+            <img src="cpim\img\logo.png" width="12%" alt="logo">
         </div>
-
-    </div>
-    <div class="media-button">
-       
-        <div ><p><a class="youtube" href="">Watch Now</a></p></div>
-    </div> 
-</section>
-
-<section class="content2">
-    <div class="video">
-    <iframe width="100%" height="300" src="https://www.youtube.com/embed/xqL0WfqxH7o" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <div class="video-title">
-        <p>Je suis un titre</p>
-    </div>
-    </div>
-    <div class="video">
-    <iframe width="100%" height="300" src="https://www.youtube.com/embed/cmpRBETOuRI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <div class="video-title">
-        <p>Je suis un titre</p>
-    </div>
-    </div>
-    <div class="video">
-    <iframe width="100%" height="300" src="https://www.youtube.com/embed/cmpRBETOuRI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <div class="video-title">
-        <p>Je suis un titre</p>
-    </div>
-    </div>
-    <div class="video">
-    <iframe width="100%" height="300" src="https://www.youtube.com/embed/AtuTytgyqvo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <div class="video-title">
-        <p>Je suis un titre</p>
-    </div>
-    </div>
-    <div class="video">
-    <iframe width="100%" height="300" src="https://www.youtube.com/embed/RBjXk-VPq8o" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <div class="video-title">
-        <p>Je suis un titre</p>
-    </div>
-    </div>
-    
-    <div class="video">
-    <iframe width="100%" height="300" src="https://www.youtube.com/embed/N2CLbUXgG-E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <div class="video-title">
-        <p>Je suis un titre</p>
-    </div>
-    </div>
-    <div class="pastor-description">
-        <div class="description-text">
-
+        <nav>
+            <ul>
+                <li><a href="http://">Home</a></li>
+                <li><a href="http://">About us</a></li>
+                <li><a href="http://">Connect</a></li>
+                <li><a href="http://">Get in touch</a></li>
+            </ul>
+        </nav>
+        <div class="button-give">
+                <p><a href="">Give</a></p>
         </div>
-        <div class="pastor-picture">
-            <img src="cpim\img\MARK11.jpg" width="20%" alt="">
-        </div>
-    </div>
-    <div class="visiting-info">
-        <div class="address">
-            <div class="overlay-address">
-                <p>Adress</p>
-                <span>
-                    6220 alder dr, Houston, TX 77081
-                </span>
+        <video class="myvideo" autoplay muted loop id="myVideo">
+            <source src="cpim\img\water.mp4" type="video/mp4">
+        </video>
+        <div class="overlay">
+            <div>
+                <p>Welcome to the</p>
+                <p id="x">Christ palace international</p>
             </div>
+
         </div>
-        <div class="services-hours">
-            <div class="overlay-services">
+        <div class="media-button">
+        
+            <div ><p><a class="youtube" href="">Watch Now</a></p></div>
+        </div> 
+    </section>
+    <div class="video-headline">
+            <h1>Recent Messages</h1>
+    </div>
+    <section class="content2">
+        <div class="video">
+            <iframe width="100%" height="300" src="https://www.youtube.com/embed/xqL0WfqxH7o" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div class="video-title">
+            <p>Je suis un titre</p>
+        </div>
+        </div>
+        <div class="video">
+            <iframe width="100%" height="300" src="https://www.youtube.com/embed/cmpRBETOuRI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div class="video-title">
+            <p>Je suis un titre</p>
+        </div>
+        </div>
+        <div class="video">
+            <iframe width="100%" height="300" src="https://www.youtube.com/embed/cmpRBETOuRI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="video-title">
+            <p>Je suis un titre</p>
+        </div>
+        </div>
+        <div class="video">
+            <iframe width="100%" height="300" src="https://www.youtube.com/embed/AtuTytgyqvo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div class="video-title">
+            <p>Je suis un titre</p>
+        </div>
+        </div>
+        <div class="video">
+            <iframe width="100%" height="300" src="https://www.youtube.com/embed/RBjXk-VPq8o" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="video-title">
+            <p>Je suis un titre</p>
+        </div>
+        </div>
+        
+        <div class="video">
+            <iframe width="100%" height="300" src="https://www.youtube.com/embed/N2CLbUXgG-E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="video-title">
+            <p>Je suis un titre</p>
+        </div>
+        </div>
+        <div class="pastor-description">
+            <div class="description-text">
 
             </div>
-            <p>Services</p>
-            <span> Sunday 10:30pm<br> Wednesday 7:30pm</span>
-        </div>
-        <div class="phone-email">
-            <div class="overlay-contact">
-
+            <div class="pastor-picture">
+                <img src="cpim\img\MARK11.jpg" width="20%" alt="">
             </div>
-            <p>Phone & Email</p>
-            <span>+3462883856<br> christpalace@gmail.com</span>
         </div>
-    </div>
-</section>
+        <div class="visiting-info">
+            <div class="address">
+                <div class="overlay-address">
+                    <p>Adress</p>
+                    <span>
+                        6220 alder dr, Houston, TX 77081
+                    </span>
+                </div>
+            </div>
+            <div class="services-hours">
+                <div class="overlay-services">
+
+                </div>
+                <p>Services</p>
+                <span> Sunday 10:30pm<br> Wednesday 7:30pm</span>
+            </div>
+            <div class="phone-email">
+                <div class="overlay-contact">
+
+                </div>
+                <p>Phone & Email</p>
+                <span>+3462883856<br> christpalace@gmail.com</span>
+            </div>
+        </div>
+    </section>
+
 </section>
     
  
